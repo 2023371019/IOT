793910166ffda23f420b8d6c686ca584af52c7e5
2023371019 <2023371019@uteq.edu.mx>
2025-03-30 15:01:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 
 // Middleware
